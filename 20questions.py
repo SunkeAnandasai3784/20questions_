bd986d01a2f5027407923e23858e715d295dac21
anandasai sunke <98510557+SunkeAnandasai3784@users.noreply.github.com>
2022-12-26 19:51:13 +0530

    Find and read 20 interview questions for Data Types, Operators, Conditional Statements, Looping Statements, Functions.

---
+++ 20questions.py
@@ -0,0 +1,20 @@
+"""What is a data type and what are the common data types in Python?
+How do you declare a variable in Python and what are the rules for naming a variable?
+What is the difference between an integer and a float data type in Python?
+What are the different types of operators in Python and how do they work?
+How do you use conditional statements (e.g. if-else, ternary operator) in Python?
+How do you use looping statements (e.g. for, while) in Python?
+What is a function and how do you define and call a function in Python?
+What is a return type and how do you specify it for a function in Python?
+What are the rules for naming a function in Python?
+How do you pass arguments to a function in Python?
+How do you handle default values for function arguments in Python?
+What is function overloading and how is it implemented in Python?
+What is recursion and how do you use it in Python?
+What is a scope and how does it work in Python?
+How do you declare and use global variables in Python?
+What is the difference between a local and a global variable in Python?
+How do you convert between data types in Python?
+What are the rules for operator precedence in Python?
+How do you use the ternary operator in Python?
+How do you handle exceptions and errors in Python?"""
